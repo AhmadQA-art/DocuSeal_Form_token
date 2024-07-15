6a6f9458908904e40bfc11f40936dfe86f391b36
AhmadQA-art <ahmadmesbahqa@gmail.com>
2024-07-15 15:42:51 +0300

    vercel changes

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const app = express();
-const port = 3001;
+const port = 3000;
 
 const API_KEY = 'vTfjbQ64sjd5jUgE4moyM6d7YcFPDWT9H88LZ8NevKG';
 
@@ -30,3 +30,42 @@ app.get('/generate-token', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+
+// const jwt = require('jsonwebtoken');
+// const API_KEY = 'vTfjbQ64sjd5jUgE4moyM6d7YcFPDWT9H88LZ8NevKG' 
+
+// function generateToken() {
+//     const payload = {
+//       user_email: 'ahmadmesbahqa@gmail.com',
+//       integration_email: 'saas-user@company.com',
+//       external_id: 'TestForm123',
+//       name: 'Integration W-9 Test Form',
+//       document_urls: ['https://www.irs.gov/pub/irs-pdf/fw9.pdf'],
+//     };
+  
+   
+//     const token = jwt.sign(payload, API_KEY);
+//     console.log(token);
+// }
+  
+//   // Wait for the jwt library to load before generating the token
+//   if (typeof jwt !== 'undefined') {
+//     generateToken();
+//   } else {
+//     window.addEventListener('load', generateToken);
+//   }
+
+
+  // Obtain from DocuSeal Console
+//vTfjbQ64sjd5jUgE4moyM6d7YcFPDWT9H88LZ8NevKG
+
+// const token = jwt.sign({
+//   user_email: 'ahmadmesbahqa@gmail.com',
+//   integration_email: 'saas-user@company.com',
+//   external_id: 'TestForm123',
+//   name: 'Integration W-9 Test Form',
+//   document_urls: ['https://www.irs.gov/pub/irs-pdf/fw9.pdf'],
+// }, API_KEY);
+
+// console.log(token)
